ee6716fcd4bb54e97e75b1e91328535002c87b39
johnbhd <villegasjohnb18@gmail.com>
2025-01-31 19:26:19 +0800

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./globals.css";
-import Home from "./page";
+import App from "./page";
 const Layout = () => {
   return (
     <html lang="en">
@@ -8,10 +8,9 @@ const Layout = () => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Notori App</title>
-
       </head>
       <body>
-          <Home />
+          <App />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-export default function Home() {
+import Home from "@/app/landing/Home"
+const App = () => {
   return (
-     <div className="flex flex-col items-center justify-center h-screen gap-y-3">
-        <h1 className="text-[5vh]">Notori App </h1>
-        <p>Build using nextjs and supabase</p>
-     </div>
+    <>
+      <Home />
+    </>
   );
-}
+};
+export default App
